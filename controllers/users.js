eebd35a9c99a34014bb54908701ecd682ceab560
jamesfulreader <jamesfulreader@gmail.com>
2021-11-02 19:06:06 -0600

    add crypto

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,12 @@
 const { createClient } = require("@supabase/supabase-js")
 const bcryptjs = require("bcryptjs")
 const dotenv = require("dotenv")
+const crypto = require("crypto"),
+  // argv = require("yargs").argv,
+  resizedIV = Buffer.allocUnsafe(16),
+  iv = crypto.createHash("sha256").update("myHashedIV").digest()
+
+iv.copy(resizedIV)
 
 dotenv.config({ path: "../config/config.env" })
 
@@ -23,8 +29,13 @@ exports.getUsers = async (req, res, next) => {
 
 exports.createUser = async (req, res, next) => {
   let password = req.body.password
-  const salt = await bcryptjs.genSalt(10)
-  const hash = await bcryptjs.hash(password, salt)
+  // const salt = await bcryptjs.genSalt(10)
+  // const hash = await bcryptjs.hash(password, salt)
+  const key = crypto.createHash("sha256").update(process.env.M_KEY).digest()
+
+  const cipher = crypto.createCipheriv("aes256", key, resizedIV)
+
+  const hash = cipher.update(password, "binary", "hex")
 
   const { data, error } = await supabase.from("User").insert([
     {
@@ -50,6 +61,13 @@ exports.createUser = async (req, res, next) => {
 
 exports.getUser = async (req, res, next) => {
   let userID = req.params.id
+  let password = req.body.password
+
+  const key = crypto.createHash("sha256").update(process.env.M_KEY).digest()
+
+  const decipher = crypto.createDecipheriv("aes256", key, resizedIV)
+
+  const pass = decipher.update(password, "hex", "binary")
 
   let { data, error } = await supabase
     .from("User")
@@ -61,21 +79,21 @@ exports.getUser = async (req, res, next) => {
   }
 
   if (data) {
-    return res.status(200).json({ data })
+    return res.status(200).json({ data, pass })
   }
 }
 
 exports.updateUser = async (req, res, next) => {
   let userID = req.params.id
 
-  let password = req.body.password
-  const salt = await bcryptjs.genSalt(10)
-  const hash = await bcryptjs.hash(password, salt)
+  const key = crypto.createHash("sha256").update(process.env.M_KEY).digest()
+
+  const cipher = crypto.createCipheriv("aes256", key, resizedIV)
 
   let { data, error } = await supabase.from("User").update({
     first_name: req.body.first_name,
     last_name: req.body.last_name,
-    password: hash,
+    password: cipher,
     username: req.body.username,
   })
 }
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,16 @@
 const express = require("express")
 
-const { getUsers, createUser, getUser } = require("../controllers/users")
+const {
+  getUsers,
+  createUser,
+  getUser,
+  updateUser,
+} = require("../controllers/users")
 
 const router = express.Router()
 
 router.route("/").get(getUsers).post(createUser)
 
-router.route("/:id").get(getUser)
+router.route("/:id").get(getUser).put(updateUser)
 
 module.exports = router
