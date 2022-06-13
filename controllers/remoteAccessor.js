776f44b2c18008742ccc9b18ced2feae4672fc72
James <jamesfulreader@gmail.com>
2022-06-13 08:41:42 -0600

    add remote access CRUD function names

--- controllers/remoteAccessor.js
+++ controllers/remoteAccessor.js
@@ -7,3 +7,13 @@ const supabase = createClient(
 	process.env.SUPABASE_URL,
 	process.env.SUPABASE_KEY
 )
+
+exports.createRemoteAccess = async (req, res, next) => {}
+
+exports.getAllRemoteAccess = async (req, res, next) => {}
+
+exports.getRemoteAccess = async (req, res, next) => {}
+
+exports.updateRemoteAccess = async (req, res, next) => {}
+
+exports.deleteRemoteAccess = async (req, res, next) => {}
--- routes/remoteAccess.js
+++ routes/remoteAccess.js
@@ -1,5 +1,20 @@
 const express = require('express')
 
+const {
+	getRemoteAccess,
+	getAllRemoteAccess,
+	createRemoteAccess,
+	updateRemoteAccess,
+	deleteRemoteAccess,
+} = require('../controllers/remoteAccessor')
+
 const router = express.Router()
+
+router.route('/').get(getAllRemoteAccess).post(createRemoteAccess)
+router
+	.route('/:id')
+	.get(getRemoteAccess)
+	.put(updateRemoteAccess)
+	.delete(deleteRemoteAccess)
 
 module.exports = router
